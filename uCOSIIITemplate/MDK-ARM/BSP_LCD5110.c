fc5b2de9b459468474fea8a4ae093d3cb0476c98
Fred_Yang <332493177@qq.com>
2016-03-21 20:57:29 +0800

    打开了PB12,13,14作为5110控制引脚

---
+++ uCOSIIITemplate/MDK-ARM/BSP_LCD5110.c
@@ -0,0 +1,7 @@
+#include <stm32f4xx_hal.h>
+
+static void BSP_LCD5110_write_byte(unsigned char dat, unsigned char command)
+{
+	//½«Æ¬Ñ¡À­µÍ
+	
+}
--- uCOSIIITemplate/Src/gpio.c
+++ uCOSIIITemplate/Src/gpio.c
@@ -84,6 +84,9 @@ void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(OTG_FS_PowerSwitchOn_GPIO_Port, OTG_FS_PowerSwitchOn_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin 
                           |Audio_RST_Pin, GPIO_PIN_RESET);
 
@@ -136,6 +139,13 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
   GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
   HAL_GPIO_Init(CLK_IN_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB12 PB13 PB14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PDPin PDPin PDPin PDPin 
                            PDPin */
